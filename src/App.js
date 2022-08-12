f331cf6fd2214a2e8bc9427ece0d6e92ea2d7496
unknown <atiqit52@gmail.com>
2022-08-12 19:37:08 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Button from './components/Button';
 import Navbar from './shared/Navbar';
-// import Navbar from './shared/Navbar/Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
     <Navbar></Navbar>
-     <h1 className='text-4xl text-red-600'>Hi Motion view </h1>
+     {/* <h1 className='text-4xl text-red-600'>Hi Motion view </h1> */}
+     
     
     </div>
   );
